d4a91aa2c11fa00de00abf99f18834358a4a0ad0
hseong3243 <hseong3243@gmail.com>
2024-01-21 23:56:45 +0900

    feat: 회원 등록 API 구현

---
+++ src/main/java/com/seong/shoutlink/domain/auth/controller/request/CreateMemberRequest.java
@@ -0,0 +1,13 @@
+package com.seong.shoutlink.domain.auth.controller.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateMemberRequest(
+    @NotBlank(message = "이메일은 공백일 수 없습니다.")
+    String email,
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+    String password,
+    @NotBlank(message = "닉네임은 공백일 수 없습니다.")
+    String nickname) {
+
+}
