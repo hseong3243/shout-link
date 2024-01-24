868cc7cee65a3d142c91e2b3e01d01b8fd5dbdcb
hseong3243 <hseong3243@gmail.com>
2024-01-24 22:29:52 +0900

    feat: ErrorCode에 예외 상황 구분을 위한 변수 추가

--- src/main/java/com/seong/shoutlink/global/exception/ErrorCode.java
+++ src/main/java/com/seong/shoutlink/global/exception/ErrorCode.java
@@ -1,15 +1,25 @@
 package com.seong.shoutlink.global.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    ILLEGAL_ARGUMENT("SL001"),
-    UNAUTHENTICATED("SL101"),
-    INVALID_ACCESS_TOKEN("SL102"),
-    EXPIRED_ACCESS_TOKEN("SL103"),
-    DUPLICATE_EMAIL("SL901"),
-    DUPLICATE_NICKNAME("SL902");
+    ILLEGAL_ARGUMENT("SL001", Constants.BAD_REQUEST),
+    UNAUTHENTICATED("SL101", Constants.UNAUTHORIZED),
+    INVALID_ACCESS_TOKEN("SL102", Constants.UNAUTHORIZED),
+    EXPIRED_ACCESS_TOKEN("SL103", Constants.UNAUTHORIZED),
+    DUPLICATE_EMAIL("SL901", Constants.CONFLICT),
+    DUPLICATE_NICKNAME("SL902", Constants.BAD_REQUEST);
 
-    private final String code;
+    private final String errorCode;
+    private final int status;
+
+    private static class Constants {
+
+        private static final int BAD_REQUEST = 400;
+        private static final int UNAUTHORIZED = 401;
+        private static final int CONFLICT = 409;
+    }
 }
