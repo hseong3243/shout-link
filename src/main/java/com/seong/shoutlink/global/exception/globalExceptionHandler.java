3e8908d4c04f0b76341a0b8672b19ea842a9033f
hseong3243 <hseong3243@gmail.com>
2024-01-24 22:30:24 +0900

    feat: 커스텀 예외를 처리하기 위한 예외 핸들러 구현

---
+++ src/main/java/com/seong/shoutlink/global/exception/globalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.seong.shoutlink.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class globalExceptionHandler {
+
+    @ExceptionHandler(ShoutLinkException.class)
+    public ResponseEntity<ErrorResponse> shoutLinkExHandle(ShoutLinkException e) {
+        ErrorResponse errorResponse
+            = new ErrorResponse(e.getMessage(), e.getErrorCode().getErrorCode());
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(errorResponse);
+    }
+}
