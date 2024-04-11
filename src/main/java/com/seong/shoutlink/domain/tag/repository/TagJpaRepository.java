25193aa22e80a60a1efde070c776c535303b7b91
hseong3243 <hseong3243@gmail.com>
2024-04-11 22:44:38 +0900

    fix: 태그 삭제 쿼리 메서드에 `@Modifying`을 추가

--- src/main/java/com/seong/shoutlink/domain/tag/repository/TagJpaRepository.java
+++ src/main/java/com/seong/shoutlink/domain/tag/repository/TagJpaRepository.java
@@ -3,13 +3,15 @@ package com.seong.shoutlink.domain.tag.repository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface TagJpaRepository extends JpaRepository<TagEntity, Long> {
 
+    @Modifying
     @Query("delete from HubTagEntity t where t.hubId=:hubId")
-    long deleteByHubId(@Param("hubId") Long hubId);
+    void deleteByHubId(@Param("hubId") Long hubId);
 
     @Query("select t from HubTagEntity t"
         + " where t.hubId=:hubId"
