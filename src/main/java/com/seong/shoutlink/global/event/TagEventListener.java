0b412842058ad3955c171da86dfc6a10643294a0
hseong3243 <hseong3243@gmail.com>
2024-05-26 21:00:45 +0900

    feat: 태그 생성 실패 시 재시도 추가

--- src/main/java/com/seong/shoutlink/global/event/TagEventListener.java
+++ src/main/java/com/seong/shoutlink/global/event/TagEventListener.java
@@ -7,8 +7,12 @@ import com.seong.shoutlink.domain.tag.service.TagUseCase;
 import com.seong.shoutlink.domain.tag.service.request.AutoCreateHubTagCommand;
 import com.seong.shoutlink.domain.tag.service.request.AutoCreateMemberTagCommand;
 import com.seong.shoutlink.domain.tag.service.response.CreateTagResponse;
+import com.seong.shoutlink.global.client.api.ApiException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -18,6 +22,11 @@ public class TagEventListener {
 
     private final TagUseCase tagUseCase;
 
+    @Retryable(
+        retryFor = ApiException.class,
+        maxAttemptsExpression = "${retry.max-attempt}",
+        backoff = @Backoff(delayExpression = "${retry.delay}",
+            multiplierExpression = "${retry.multiply}"))
     @Async(value = "generativeTaskExecutor")
     @TransactionalEventListener
     public void createHubTags(CreateHubLinkEvent event) {
@@ -30,6 +39,11 @@ public class TagEventListener {
         }
     }
 
+    @Retryable(
+        retryFor = ApiException.class,
+        maxAttemptsExpression = "${retry.max-attempt}",
+        backoff = @Backoff(delayExpression = "${retry.delay}",
+            multiplierExpression = "${retry.multiply}"))
     @Async(value = "generativeTaskExecutor")
     @TransactionalEventListener
     public void createMemberTags(CreateMemberLinkEvent event) {
@@ -40,5 +54,11 @@ public class TagEventListener {
         } catch (NotMetCondition e) {
             log.debug("[Event] 회원 태그 자동 생성 취소. 조건을 만족하지 않음.");
         }
+    }
+
+    @Recover
+    public void recover(RuntimeException e) {
+        log.error("[Event] 태그 자동 생성 실패.", e);
+        throw e;
     }
 }
